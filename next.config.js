5bfd5e9f4ddb30e94f32fcea0a2b0407aad89368
sanskar1309 <rmaharwade@gmail.com>
2025-11-09 16:31:19 +0530

    feat: deployment ready

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: '/Users/ranium/Desktop/health/holistic-health-buddy',
   images: {
     domains: ['localhost'],
   },
